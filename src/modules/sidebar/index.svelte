961ef2f1155d3960804e2044b923c76f088c465c
YErii <yerii@Yggdrasill.local>
2022-04-26 23:56:07 +0800

    feat: sibebar styles

--- src/modules/sidebar/index.svelte
+++ src/modules/sidebar/index.svelte
@@ -1,34 +1,22 @@
 <script lang="ts">
-  let count: number = 0
-  const increment = () => {
-    count += 1
-  }
 </script>
 
-<button on:click={increment}>
-  Clicks: {count}
-</button>
+<div id="container">
+  <div>app1</div>
+  <div>app2</div>
+  <div>app3</div>
+  <div>+</div>
+</div>
 
 <style>
-  button {
-    font-family: inherit;
-    font-size: inherit;
-    padding: 1em 2em;
-    color: #ff3e00;
-    background-color: rgba(255, 62, 0, 0.1);
-    border-radius: 2em;
-    border: 2px solid rgba(255, 62, 0, 0);
-    outline: none;
-    width: 200px;
-    font-variant-numeric: tabular-nums;
-    cursor: pointer;
-  }
+  #container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: start;
 
-  button:focus {
-    border: 2px solid #ff3e00;
-  }
-
-  button:active {
-    background-color: rgba(255, 62, 0, 0.2);
+    width: 48px;
+    height: calc(100vh - 48px);
+    overflow-y: auto;
   }
 </style>
