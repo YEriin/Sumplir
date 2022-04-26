dbc47e802059105f6bb7e107ed365d0abf7f9951
YErii <yerii@Yggdrasill.local>
2022-04-27 00:17:12 +0800

    feat: sibebar items and auto flow y

--- src/modules/sidebar/index.svelte
+++ src/modules/sidebar/index.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+  const workspaces = Array.from({ length: 30 }, (_, idx) => ({ name: "W" }));
 </script>
 
 <div id="container">
-  <div>app1</div>
-  <div>app2</div>
-  <div>app3</div>
+  {#each workspaces as { name }, idx}
+    <div class="workspaceItem">{name + idx}</div>
+  {/each}
   <div>+</div>
 </div>
 
@@ -14,9 +15,21 @@
     flex-direction: column;
     align-items: center;
     justify-content: start;
+    gap: 10px;
 
-    width: 48px;
+    width: 64px;
     height: calc(100vh - 48px);
     overflow-y: auto;
+
+    border-right: 1px solid rgb(99, 154, 17);
+
+    padding: 10px 0;
+  }
+
+  .workspaceItem {
+    width: 48px;
+    height: 48px;
+    border: 1px solid rgb(121, 25, 25);
+    flex-shrink: 0;
   }
 </style>
