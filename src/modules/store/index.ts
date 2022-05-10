28b382ec2fecba5edf98602606528497b164e96e
YErii <yerii@Yggdrasill.local>
2022-05-10 21:29:18 +0800

    feat: add store types

--- src/modules/store/index.ts
+++ src/modules/store/index.ts
@@ -3,7 +3,22 @@ import { writable } from "svelte/store";
 const workspaces = Array.from({ length: 30 }, (_, idx) => `W${idx}`);
 const boards = Array.from({ length: 30 }, (_, idx) => `B${idx}`);
 
-export const workspaceStore = writable({
+interface Board {
+  name: string;
+}
+
+interface Workspace {
+  name: string;
+  boards: Board[];
+}
+
+interface Store {
+  createdAt: string;
+  currentWorkspaceKey: string;
+  workspaces: Workspace[];
+}
+
+export const workspaceStore = writable<Store>({
   createdAt: new Date().toISOString(),
   currentWorkspaceKey: null,
   workspaces: workspaces.map((name) => ({
