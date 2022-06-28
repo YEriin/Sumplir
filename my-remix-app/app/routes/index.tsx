34de73d9da77ecf96a2bed399054c9c79df832b3
YErii <yerii@Yggdrasill.local>
2022-06-28 23:49:55 +0800

    feat: import and use whiteboard styles

--- my-remix-app/app/routes/index.tsx
+++ my-remix-app/app/routes/index.tsx
@@ -1,9 +1,9 @@
 import Nav, { links as NavLinks } from "../components/nav";
 import Sidebar, { links as SidebarLinks } from "../components/sidebar";
-import WhiteBoard from "../components/whiteboard";
+import WhiteBoard, { links as WhiteboardLinks } from "../components/whiteboard";
 
 export function links() {
-  return [...NavLinks(), ...SidebarLinks()];
+  return [...NavLinks(), ...SidebarLinks(), ...WhiteboardLinks()];
 }
 
 export default function Index() {
